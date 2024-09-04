d47dac80dd030463c0dec1c1a8e82354454f7df8
AV_Ciobanu <155495497+AlinV15@users.noreply.github.com>
2024-09-04 16:50:27 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        customColor: '#8a5050',
+      }
+    },
+  },
+  plugins: [],
+}
+
